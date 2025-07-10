5a343342dd941e395596c25229b01c768ac1dffa
morningB <yjw001205@gmail.com>
2025-07-10 19:29:28 +0900

    feat: 커브볼 로직 구체화

--- Assets/Baseball Game/Scripts/CurveBall.cs
+++ Assets/Baseball Game/Scripts/CurveBall.cs
@@ -2,10 +2,29 @@ using UnityEngine;
 
 public class Curveball : Ball
 {
+    private float gravity = 9.81f;
+    private Vector3 velocity;
+    private Vector3 curveDir = Vector3.left;
+    private float curveStrength = 0.3f;
+
+    public override void Throw(Vector3 throwDirection)
+    {
+        velocity = throwDirection.normalized * ballData.speed;
+    }
+
     protected override void Move()
     {
-        Vector3 curvedDir = direction + ballData.curveDirection * Mathf.Sin(Time.time * ballData.speed) * ballData.curveStrength;
-        curvedDir.Normalize();
-        transform.position += curvedDir * ballData.speed * Time.deltaTime;
+        // 중력 가속도 적용 (Y축 방향 아래로)
+        velocity += Vector3.down * gravity * Time.deltaTime;
+
+        // 커브 회전 가속도 (좌측으로 휘는 효과)
+        Vector3 curvedVelocity = velocity + curveDir * curveStrength;
+
+        // 위치 업데이트
+        transform.position += curvedVelocity * Time.deltaTime;
+
+        // 방향 업데이트
+        direction = curvedVelocity.normalized;
     }
+
 }
