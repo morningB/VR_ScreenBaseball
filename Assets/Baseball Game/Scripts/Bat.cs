d23f251b79999b451b345a05a2674127c906627c
morningB <yjw001205@gmail.com>
2025-07-10 19:29:00 +0900

    style: 필요없는 코드 추가

--- Assets/Baseball Game/Scripts/Bat.cs
+++ Assets/Baseball Game/Scripts/Bat.cs
@@ -15,11 +15,12 @@ public class Bat : MonoBehaviour, IHittable
       Rigidbody ballRb = ball.GetComponent<Rigidbody>();
       if (ballRb != null)
       {
+         ball.direction = Vector3.zero;
          // 배트 스윙 속도에 따라 반사력 보강
          Vector3 reflectedDir = Vector3.Reflect(ball.direction, transform.forward);
-
+         
          ballRb.linearVelocity = reflectedDir.normalized * ballRb.linearVelocity.magnitude * swingSpeed;
-
+         
          // 순간 힘도 추가해 튕김 강화
          ballRb.AddForce(reflectedDir.normalized * swingSpeed * 10f, ForceMode.Impulse);
       }
